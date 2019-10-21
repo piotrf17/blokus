4890278de9893e238eab1a5812c6850c1d644f8c
Piotr Fidkowski <piotr@fidkowski.com>
2019-10-20 23:50:21 -0400

    Add benchmark result

--- ai/mcts_benchmark.cc
+++ ai/mcts_benchmark.cc
@@ -5,6 +5,19 @@
 namespace blokus {
 namespace {
 
+// Results as of 2019/10/20:
+//
+// Run on (4 X 3400 MHz CPU s)
+// CPU Caches:
+//   L1 Data 32K (x2)
+//   L1 Instruction 32K (x2)
+//   L2 Unified 256K (x2)
+//   L3 Unified 4096K (x1)
+// Load Average: 1.14, 1.01, 0.99
+// -----------------------------------------------------
+// Benchmark           Time             CPU   Iterations
+// -----------------------------------------------------
+// BM_Rollout  115316947 ns    115194159 ns            6
 static void BM_Rollout(benchmark::State& state) {
   Game game;
   for (auto _ : state) {
