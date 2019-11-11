07f6187d389f25ff70d45ed039bb79da7832bc70
Piotr Fidkowski <piotr@fidkowski.com>
2019-11-10 21:58:57 -0500

    Add trivial test for Board::IsPossible

--- game/board_test.cc
+++ game/board_test.cc
@@ -8,8 +8,23 @@ namespace {
 
 using ::testing::Eq;
 
-TEST(BoardTest, Foo) {
-  EXPECT_THAT(2, Eq(3));
+TEST(BoardTest, IsPossible) {
+  Board board;
+  // The 1x1 tile should be possible at the start in any orientation, but
+  // not anywhere else on the board.
+  for (int rotation = 0; rotation < 4; rotation++) {
+    for (bool flip : {false, true}) {
+      for (uint8_t x = 0; x < Board::kWidth; ++x) {
+        for (uint8_t y = 0; y < Board::kHeight; ++y) {
+          if (x == 0 && y == 0) {
+            EXPECT_TRUE(board.IsPossible({BLUE, 0, {{x, y}, rotation, flip}}));
+          } else {
+            EXPECT_FALSE(board.IsPossible({BLUE, 0, {{x, y}, rotation, flip}}));
+          }
+        }
+      }
+    }
+  }
 }
 
 }  // namespace
